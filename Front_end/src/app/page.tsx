151cc752187546d69bb1d669437312114afed263
ayarinesrine <nayari@zeta-technologie.com>
2024-05-02 09:54:37 +0100

    up

--- Front_end/src/app/page.tsx
+++ Front_end/src/app/page.tsx
@@ -1,14 +1,20 @@
 import CardProfile from "@/shared/CardProfile/CardProfile";
-
 function page() {
-  return <div className="w-defaultwidth mx-auto flex  ">
-
-    <div className="basis-2/3">jjjj</div>
-    <div className="basis-1/3 ">
-      <CardProfile />
+  const profile = {
+    img: "/assets/fackUser.jpg",
+    name: "string",
+    job: "string",
+    address: "string",
+    birth: "string",
+  };
+  return (
+    <div className="w-defaultwidth mx-auto flex  ">
+      <div className="basis-2/3"></div>
+      <div className="basis-1/3 ">
+        <CardProfile data={profile} />
+      </div>
     </div>
-
-  </div>;
+  );
 }
 
 export default page;
--- Front_end/src/shared/CardProfile/CardProfile.tsx
+++ Front_end/src/shared/CardProfile/CardProfile.tsx
@@ -1,35 +1,73 @@
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function CardProfile(props: {
-    data?: {
-        img: string
-        name: string
-        job: string
-        address: string
-        birth: string
-    }
+  data: {
+    img?: string;
+    name?: string;
+    job?: string;
+    address?: string;
+    birth?: string;
+  };
 }) {
-    return (
-        <div className='w-defaultwidth m-auto'>
-            <div className="bg-blue rounded-xl text-white p-3">My profile</div>
-            <div className="flex w-full ">
-                <div className="basis-2/6">
-                    <img src={props.data?.img} className="rounded-full" alt={props.data?.name} />
-                </div>
-                <div className="basis-4/6 ">
-                    <div className="">{props.data?.name}</div>
-                    <div className="">{props.data?.job}</div>
-                    <div className="">{props.data?.address}</div>
-                </div>
-            </div>
-            <div className="flex">
-                <div className="basis-1/3">{props.data?.birth}</div>
-                <hr />
-                <div className="basis-1/3">{props.data?.birth}</div>
-                <hr />
-                <div className="basis-1/3">{props.data?.birth}</div>
-            </div>
+  return (
+    <div className="w-defaultwidth m-auto bg-white rounded-xl">
+      <div className="bg-blue rounded-xl text-white p-3 py-4">My profile</div>
+      <div className="flex w-full place-items-center py-4">
+        <div className="basis-2/6 flex ">
+          <div className="m-auto">
+            {props.data.img !== undefined && props.data.img !== "" ? (
+              <img
+                src={props.data?.img}
+                className="rounded-full w-24"
+                alt={props.data?.name}
+              />
+            ) : (
+              <div className="w-24  bg-lightBlue rounded-full ">
+                <FontAwesomeIcon
+                  className="w-20 p-4 m-auto text-blue"
+                  icon={faUser}
+                />
+              </div>
+            )}
+          </div>
         </div>
-    )
+        <div className="basis-4/6 flex">
+          <div className="me-auto">
+            <div className="font-bold text-2xl">{props.data?.name}</div>
+            <div className="text-gray-500">{props.data?.job}</div>
+            <div className="flex">
+              <FontAwesomeIcon icon={faLocationDot} className="w-4 text-blue" />
+              <div className="ps-2">{props.data?.address}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex py-3">
+        <div className="basis-1/3 flex">
+          <div className="m-auto">
+            <div className="text-gray-500">gg</div>
+            <div>{props.data?.birth}</div>
+          </div>
+        </div>
+        <hr className="border-[1px] border-lightPurple h-10" />
+        <div className="basis-1/3 flex">
+          <div className="m-auto">
+            <div className="text-gray-500">rrr</div>
+            <div>{props.data?.birth}</div>
+          </div>
+        </div>
+        <hr className="border-[1px] border-lightPurple h-10" />
+        <div className="basis-1/3 flex ">
+          <div className="m-auto">
+            <div className="text-gray-500">eee</div>
+            <div>{props.data?.birth}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default CardProfile
+export default CardProfile;
