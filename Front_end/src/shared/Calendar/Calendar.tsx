4ad8d6e015495a8f01dbd1dafa7c7db5a6ec4e5e
ayarinesrine <nayari@zeta-technologie.com>
2024-05-02 14:21:50 +0100

    up

--- Front_end/src/shared/Calendar/Calendar.tsx
+++ Front_end/src/shared/Calendar/Calendar.tsx
@@ -8,11 +8,13 @@ function Calendar() {
   const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   const currentDate = new Date();
   const dayOfWeek = daysOfWeek[currentDate.getDay()];
+  const month = currentDate.toLocaleString("en-US", { month: "long" });
+  const day = currentDate.toLocaleString("en-US", { day: "numeric" });
   return (
     <div className="w-defaultwidth m-auto rounded-xl bg-gradient-to-t from-white to-lightPurple  mt-10">
       <div className="bg-blue rounded-xl text-white p-3 py-4 flex">
         <div className="basis-1/2 m-auto"> My Calendar</div>
-        <a href="/profile" className="basis-1/2 flex ">
+        <a href="/" className="basis-1/2 flex ">
           <div className="flex ms-auto bg-jordyBlue rounded-lg p-2 px-4">
             <div className="pe-2 "> {date}</div>
             <FontAwesomeIcon icon={faCalendarDays} className="md:h-6 h-4 " />
@@ -33,7 +35,11 @@ function Calendar() {
           </div>
         ))}
       </div>
-      <div className="bg-white p-3 rounded-b-xl">kkk</div>
+      <div className="bg-white p-3 rounded-b-xl  ">
+        <div className="text-blue">
+          {day} {month} :
+        </div>
+      </div>
     </div>
   );
 }
