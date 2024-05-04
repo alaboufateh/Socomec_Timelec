afa402fbb26675eddf63470d961de37480058600
Ayari Nesrine <ayari.nessrine3@gmail.com>
2024-05-04 20:28:44 +0100

    taw taw

--- Front_end/src/app/profile/page.tsx
+++ Front_end/src/app/profile/page.tsx
@@ -3,7 +3,7 @@ import Home from "@/app/Hom/page";
 
 function page() {
   return <div>
-    <p className="bg-blue">ena nabil</p>
+    <p className="bg-blue">ena nabil jjjjjjjjjjjjjjjjjj</p>
     <Home />
   </div>;
 }
