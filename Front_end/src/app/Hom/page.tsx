d353e8118bf6ca38c8b6b73fbfbabd0041f03c05
Ayari Nesrine <ayari.nessrine3@gmail.com>
2024-05-05 12:04:02 +0100

    up

--- Front_end/src/app/Hom/page.tsx
+++
@@ -1,7 +0,0 @@
-function home() {
-    return (
-        <div className='bg-red-700'>ena home walit page</div>
-    )
-}
-
-export default home
--- Front_end/src/app/profile/page.tsx
+++ Front_end/src/app/profile/page.tsx
@@ -1,11 +1,8 @@
-import Home from "@/app/Hom/page";
-
 
 function page() {
-  return <div>
-    <p className="bg-blue">ena nabil jjjjjjjjjjjjjjjjjj</p>
-    <Home />
-  </div>;
+  return (
+    <div>page</div>
+  )
 }
 
-export default page;
+export default page
