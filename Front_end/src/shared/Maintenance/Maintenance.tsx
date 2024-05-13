ca79712d59137456c152dc51b61194f096c24696
ayarinesrine <nayari@zeta-technologie.com>
2024-05-13 14:44:01 +0100

    up

--- Front_end/src/shared/Maintenance/Maintenance.tsx
+++ Front_end/src/shared/Maintenance/Maintenance.tsx
@@ -8,7 +8,7 @@ function Maintenance() {
       <div className="flex">
         <div className="basis-1/2  ">
           <div className="text-blue py-1"> Bel :</div>
-          <img className="w-60 m-auto" src="/assets/machine3.png" />
+          <img className="w-52 m-auto" src="/assets/machine1.png" />
         </div>
 
         <div className="basis-1/2">
